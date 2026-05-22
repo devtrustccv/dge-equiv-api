b1148a0f4d8ea7e8bca852f2bf19233ae9403625
Adnilson Barbosa <130472830+AdnilsonBarbosa@users.noreply.github.com>
2026-05-22 10:58:22 -0100

    add novo dados para reporter cerficado

--- src/main/java/dge/dge_equiv_api/application/process/service/ProcessService.java
+++ src/main/java/dge/dge_equiv_api/application/process/service/ProcessService.java
@@ -41,7 +41,7 @@ public class ProcessService {
         preencherDadosRequerente(dto, requerente);
         preencherDadosPedidos(dto, pedidos);
 
-        System.out.println("dto"+dto);
+        log.info("saida dto ....."+ dto);
 
         Map<String, String> headers = new HashMap<>();
         headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
